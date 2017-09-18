42e1dd6cdd51b0f40a0a0b5f91658cd3e7b58c3e
zicohasan <zico_hasan@yahoo.com>
2017-09-18 14:30:38 +0100

    Initial commit

---
+++ app/src/main/java/mp/zico/org/sqlchart/MainActivity.java
@@ -0,0 +1,81 @@
+package mp.zico.org.sqlchart;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class MainActivity extends AppCompatActivity {
+    Button insertButton;
+    EditText inputTextX, inputTextY;
+    GraphView graphView;
+    LineGraphSeries<DataPoint> series=new LineGraphSeries<>(new DataPoint[0]);
+    MyHelper myHelper;
+    SQLiteDatabase sqLiteDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        insertButton=(Button) findViewById(R.id.button);
+        inputTextX=(EditText) findViewById(R.id.inputTextX);
+        inputTextY=(EditText) findViewById(R.id.inputTextY);
+        graphView=(GraphView) findViewById(R.id.graph);
+
+        myHelper=new MyHelper(this);
+        sqLiteDatabase=myHelper.getWritableDatabase();
+
+        exqButton();
+//        LineGraphSeries<DataPoint> series=new LineGraphSeries<>(getDataPoint());
+//        graphView.addSeries(series);
+    }
+
+    private void exqButton() {
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int xVal=Integer.parseInt(String.valueOf(inputTextX.getText()));
+                int yVal=Integer.parseInt(String.valueOf(inputTextY.getText()));
+
+                myHelper.insertData(xVal,yVal);
+                series.resetData(getDataPoint());
+//                series=new LineGraphSeries<DataPoint>(getData());
+                graphView.addSeries(series);
+            }
+        });
+    }
+
+
+
+    private DataPoint[] getDataPoint() {
+        // Read Data from database
+        String[] columns={"xValues","yValues"};
+        Cursor cursor=sqLiteDatabase.query("MyTable",columns,null,null,null,null,null);
+        DataPoint[] dp=new DataPoint[cursor.getCount()];
+        for (int i=0;i<=cursor.getCount();i++)
+        {
+            cursor.moveToNext();
+            dp[i]=new DataPoint(cursor.getInt(0), cursor.getInt(1));
+        }
+        return dp;
+    }
+
+
+/*    public DataPoint[] getDataPoint() {
+        DataPoint[] dp=new DataPoint[]{
+                new DataPoint(0,1),
+                new DataPoint(2,5),
+                new DataPoint(3,1),
+                new DataPoint(5,6),
+                new DataPoint(8,3)
+        };
+        return (dp);
+    }*/
+}
---
+++ app/src/main/java/mp/zico/org/sqlchart/MyHelper.java
@@ -0,0 +1,43 @@
+package mp.zico.org.sqlchart;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by zico on 17/09/2017.
+ */
+
+public class MyHelper extends SQLiteOpenHelper {
+    private Context con;
+    public MyHelper(Context context) {
+        super(context, "MyDatabase", null, 1);
+        con=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String createTable="create table myTable (xValues INTEGER, yValues INTEGER);";
+        db.execSQL(createTable);
+//        Toast.makeText(con, "Table Created", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insertData(int valX, int valY){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+
+        contentValues.put("xValues",valX);
+        contentValues.put("yValues",valY);
+        db.insert("myTable", null, contentValues);
+//        Toast.makeText(con, "Data Inserted", Toast.LENGTH_LONG).show();
+    }
+
+}
